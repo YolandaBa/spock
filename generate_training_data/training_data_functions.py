75bbce49b35bd64b946e304682f05db80da99169
YolandaBa <yba@hmc.edu>
2023-03-13 14:55:13 -0700

    changes for new python/dask versions

--- generate_training_data/training_data_functions.py
+++ generate_training_data/training_data_functions.py
@@ -2,7 +2,10 @@ import rebound
 import numpy as np
 import pandas as pd
 import dask.dataframe as dd
-from spock.simsetup import init_sim_parameters
+import sys
+sys.path.append('../spock')
+sys.path.append('../../spock')
+from simsetup import init_sim_parameters
 
 def training_data(row, safolder, runfunc, args):
     try:
@@ -28,7 +31,7 @@ def gen_training_data(outputfolder, safolder, runfunc, args):
     df = pd.read_csv(outputfolder+"/runstrings.csv", index_col = 0)
     ddf = dd.from_pandas(df, npartitions=48)
     testres = training_data(df.loc[0], safolder, runfunc, args) # Choose formatting based on selected runfunc return type
-
+    
     metadf = pd.DataFrame([testres]) # make single row dataframe to autodetect meta
-    res = ddf.apply(training_data, axis=1, meta=metadf, args=(safolder, runfunc, args)).compute(scheduler='processes')
+    res = ddf.apply(training_data, axis=1, meta=metadf, args=(safolder, runfunc, args)).compute()
     res.to_csv(outputfolder+'/trainingdata.csv')
